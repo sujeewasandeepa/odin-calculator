eb49f73aaf115c29d4e055a1d6a34a9ceeaaa4f3
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-01-12 00:15:29 +0530

    Add operate function
    operate function handles all the
    operators, as needed.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// basic functions
 function add (a, b) {
     return a + b;
 }
@@ -10,8 +11,32 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b != 0) {
         return a / b;
+    }else{
+        return "can't divide by zero.";
     }
 }
+
+function operate(operator, num1, num2) {
+    if (operator === "addition") {
+        return add(num1, num2);
+    }
+    else if (operator === "substraction") {
+        return substract(num1, num2);
+    }
+    else if (operator === "multiplication") {
+        return multiply(num1, num2);
+    }
+    else if (operator === "division") {
+        return divide(num1, num2);
+    }
+    else {
+        return "something is wrong";
+    }
+}
+
+
+// ***         testing         *** //
+
 
 // testing add function
 const sum = add(3, 4);
@@ -28,3 +53,13 @@ console.log(product);
 // testing divide function
 const quotient = divide(100, 5);
 console.log(quotient);
+const quotient_ = divide(10, 0);
+console.log(quotient_);
+
+// testing the operate function
+const operated1 = operate("addition", 5, 2);
+console.log("operated 1: " + operated1);
+const operatedDivisionNonZero = operate("division", 10, 2);
+console.log(operatedDivisionNonZero);
+const operatedDivisionZero = operate("division", 5, 0);
+console.log(operatedDivisionZero);
