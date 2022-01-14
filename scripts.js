e3ab904653e3335fe72d1c4244f068a3195d7a59
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-01-14 17:04:01 +0530

    implemented addition prototype

--- scripts.js
+++ scripts.js
@@ -1,33 +1,113 @@
-const clearBtn     = document.querySelector('#clearBtn');
-const quotientBtn  = document.querySelector('#quotientBtn');
-const divisionBtn  = document.querySelector('#divisionBtn');
-const multiplyBtn  = document.querySelector('#multiplyBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const quotientBtn = document.querySelector('#quotientBtn');
+const divisionBtn = document.querySelector('#divisionBtn');
+const multiplyBtn = document.querySelector('#multiplyBtn');
 const substractBtn = document.querySelector('#substractBtn');
-const addBtn       = document.querySelector('#addBtn');
-const equalBtn     = document.querySelector('#equalBtn');
+const addBtn = document.querySelector('#addBtn');
+const equalBtn = document.querySelector('#equalBtn');
+
+const specialButtons = document.querySelectorAll('.specialButtons');
 
 const sevenBtn = document.querySelector('#sevenBtn');
 const eightBtn = document.querySelector('#eightBtn');
-const nineBtn  = document.querySelector('#nineBtn');
-const fourBtn  = document.querySelector('#fourBtn');
-const fiveBtn  = document.querySelector('#fiveBtn');
-const sixBtn   = document.querySelector('#sixBtn');
-const oneBtn   = document.querySelector('#oneBtn');
-const twoBtn   = document.querySelector('#twoBtn');
+const nineBtn = document.querySelector('#nineBtn');
+const fourBtn = document.querySelector('#fourBtn');
+const fiveBtn = document.querySelector('#fiveBtn');
+const sixBtn = document.querySelector('#sixBtn');
+const oneBtn = document.querySelector('#oneBtn');
+const twoBtn = document.querySelector('#twoBtn');
 const threeBtn = document.querySelector('#threeBtn');
-const zeroBtn  = document.querySelector('#zeroBtn');
+const zeroBtn = document.querySelector('#zeroBtn');
 
 const displayArea = document.querySelector('#displayArea');
+const buttons = document.querySelector('button');
 
+let num1 = 0;
+let addBtnPressed = false;
+
+
+// implementing buttons
+// numerics (0 - 9)
 
 oneBtn.addEventListener('click', () => {
-    displayArea.innerText = '1';
+    displayArea.value += oneBtn.value; enableButtons();
+});
+twoBtn.addEventListener('click', () => {
+    displayArea.value += twoBtn.value; enableButtons();
+});
+threeBtn.addEventListener('click', () => {
+    displayArea.value += threeBtn.value; enableButtons();
+});
+fourBtn.addEventListener('click', () => {
+    displayArea.value += fourBtn.value; enableButtons();
+});
+fiveBtn.addEventListener('click', () => {
+    displayArea.value += fiveBtn.value; enableButtons();
+});
+sixBtn.addEventListener('click', () => {
+    displayArea.value += sixBtn.value; enableButtons();
+});
+sevenBtn.addEventListener('click', () => {
+    if (addBtnPressed) {
+        addBtnPressed = false;
+        const sum = operate('+', num1, sevenBtn.value);
+        displayArea.value = sum;
+        enableButtons();
+    } else {
+        displayArea.value += sevenBtn.value; enableButtons();
+    }
+});
+eightBtn.addEventListener('click', () => {
+    displayArea.value += eightBtn.value; enableButtons();
+});
+nineBtn.addEventListener('click', () => {
+    displayArea.value += nineBtn.value; enableButtons();
+});
+zeroBtn.addEventListener('click', () => {
+    displayArea.value += zeroBtn.value; enableButtons();
 });
 
 
+// operations
+// implementing addition
+addBtn.addEventListener('click', () => {
+    num1 = displayArea.value;
+    disableButtons();
+    addBtnPressed = true;
+})
+
+// clear button
+clearBtn.addEventListener('click', () => displayArea.value = '');
+
+
+// operate function
+// this function manages all the numerical operations
+// for any two numbers.
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    }
+    else if (operator === "-") {
+        return substract(num1, num2);
+    }
+    else if (operator === "*") {
+        return multiply(num1, num2);
+    }
+    else if (operator === "/") {
+        return divide(num1, num2);
+    }
+    else if (operator === "%") {
+        return getQuotient(num1, num2);
+    }
+    else {
+        return "something is wrong";
+    }
+}
+
+
 // basic functions
-function add (a, b) {
-    return a + b;
+function add(a, b) {
+    return parseInt(a) + parseInt(b);
 }
 function substract(a, b) {
     return a - b;
@@ -38,34 +118,25 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b != 0) {
         return a / b;
-    }else{
+    } else {
         return "can't divide by zero.";
     }
 }
-
-function operate(operator, num1, num2) {
-    if (operator === "addition") {
-        return add(num1, num2);
-    }
-    else if (operator === "substraction") {
-        return substract(num1, num2);
-    }
-    else if (operator === "multiplication") {
-        return multiply(num1, num2);
-    }
-    else if (operator === "division") {
-        return divide(num1, num2);
-    }
-    else {
-        return "something is wrong";
-    }
+function getQuotient(a, b) {
+    return a % b;
 }
 
 
-function populateDisplay() {
-    
+function disableButtons() {
+    for (let i = 0; i < specialButtons.length; i++) {
+        specialButtons[i].disabled = true;
+    }
 }
-
+function enableButtons() {
+    for (let i = 0; i < specialButtons.length; i++) {
+        specialButtons[i].disabled = false;
+    }
+}
 
 
 
@@ -93,9 +164,9 @@ const quotient_ = divide(10, 0);
 console.log(quotient_);
 
 // testing the operate function
-const operated1 = operate("addition", 5, 2);
+const operated1 = operate("+", 5, 2);
 console.log("operated 1: " + operated1);
-const operatedDivisionNonZero = operate("division", 10, 2);
+const operatedDivisionNonZero = operate("/", 10, 2);
 console.log(operatedDivisionNonZero);
-const operatedDivisionZero = operate("division", 5, 0);
+const operatedDivisionZero = operate("/", 5, 0);
 console.log(operatedDivisionZero);
